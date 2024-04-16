a85186eb340d619f9d008fa4f4f2ea9abd3352c8
zoeysears <zoey.mae.sears@gmail.com>
2024-04-15 22:21:04 -0500

    get/post & validators

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,8 +4,8 @@ import { AppComponent } from './app/app.component';
 import { provideAnimations} from '@angular/platform-browser/animations'
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes'
-
+import { provideHttpClient } from '@angular/common/http';
 bootstrapApplication(AppComponent,{
-  providers: [provideAnimations(), provideRouter(routes)]
+  providers: [provideAnimations(), provideRouter(routes), provideHttpClient()]
 })
   .catch((err) => console.error(err));
